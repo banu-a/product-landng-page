9649b8231cf50aff76d7d1fbe909e8cea342c806
Banu AĞGÜN <37499235+banuaggun@users.noreply.github.com>
2019-03-21 01:35:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-/*const navbarToggler=document.querySelector(".navbar-toggler");
-const navbarMenu=document.querySelector(".navbar ul");
-const navbarLinks=document.querySelectorAll(".navbar a");
-
-navbarToggler.addEventListener("click", navbarTogglerClick);
-
-function navbarTogglerClick(){
-	navbarToggler.classList.toggle("open-navbar-toggler");
-	navbarMenu.classList.toggle("open");
-}
-
-navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
-
-function navbarLinkClick(){
-	if(navbarMenu.classList.contains("open")){
-		navbarTogglerClick();
-	}
-}
-
-
-$(document).ready(function(){
-    $(".navbar nav a").click(function(){
-      $(this).tab('show');
-    });
-	});
-	
-*/
-
 $(document).ready(function(){
 $('.form').find('input, textarea').on('keyup blur focus', function (e) {
   
